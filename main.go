e7ea0a6a7d7e31f48c31f7b8d0f0b76af603445a
Roberto Ortega <berto.ort@gmail.com>
2015-11-06 17:06:08 -0700

    solve and print methods added

--- main.go
+++ main.go
@@ -4,12 +4,6 @@ import (
 	"fmt"
 )
 
-// Sudoku methods
-// type Sudoku interface {
-// 	createPuzzle()
-// 	getPuzzle() [9][9]square
-// }
-
 // Puzzle class
 type Puzzle struct {
 	board [9][9]square
@@ -20,14 +14,96 @@ type square struct {
 	x   int
 	y   int
 	box int
+	not []int
 }
 
 func main() {
 	newBoard := Puzzle{}
-	fmt.Println(newBoard.createPuzzle())
+	newBoard.createPuzzle()
+	var input [9][9]int
+	input[0] = [9]int{1, 0, 3, 0, 0, 6, 0, 8, 0}
+	input[1] = [9]int{0, 5, 0, 0, 8, 0, 1, 2, 0}
+	input[2] = [9]int{7, 0, 9, 1, 0, 3, 0, 5, 6}
+	input[3] = [9]int{0, 3, 0, 0, 6, 7, 0, 9, 0}
+	input[4] = [9]int{5, 0, 7, 8, 0, 0, 0, 3, 0}
+	input[5] = [9]int{8, 0, 1, 0, 3, 0, 5, 0, 7}
+	input[6] = [9]int{0, 4, 0, 0, 7, 8, 0, 1, 0}
+	input[7] = [9]int{6, 0, 8, 0, 0, 2, 0, 4, 0}
+	input[8] = [9]int{0, 1, 2, 0, 4, 5, 0, 7, 8}
+	newBoard.fillPuzzle(input)
+	newBoard.solve()
+	board := newBoard.display()
+	fmt.Println(board)
 }
 
-func (p Puzzle) createPuzzle() [9][9]square {
+func (p *Puzzle) display() [9][9]int {
+	var board [9][9]int
+	for i, row := range p.board {
+		var newRow [9]int
+		for j := range row {
+			newRow[j] = p.board[i][j].val
+		}
+		board[i] = newRow
+	}
+	return board
+}
+
+func (p *Puzzle) solve() {
+	for i, row := range p.board {
+		for j := range row {
+			p.board[i][j].check(p)
+		}
+	}
+}
+
+func (s *square) check(p *Puzzle) {
+	for i, row := range p.board {
+		for j := range row {
+			if s.val == 0 && p.board[i][j].val != 0 {
+				if s.x != p.board[i][j].x || s.y != p.board[i][j].y {
+					s.compare(p.board[i][j])
+				}
+			}
+			if len(s.not) == 8 {
+				list := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+				var n int
+				for _, v := range list {
+					if !numInSlice(v, s.not) {
+						n = v
+					}
+				}
+				s.val = n
+			}
+		}
+	}
+}
+
+func numInSlice(num int, list []int) bool {
+	for _, v := range list {
+		if v == num {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *square) compare(c square) {
+	if s.x == c.x || s.y == c.y {
+		s.not = append(s.not, c.val)
+	} else if s.box == c.box {
+		s.not = append(s.not, c.val)
+	}
+}
+
+func (p *Puzzle) fillPuzzle(input [9][9]int) {
+	for i, row := range p.board {
+		for j := range row {
+			p.board[i][j].val = input[i][j]
+		}
+	}
+}
+
+func (p *Puzzle) createPuzzle() [9][9]square {
 	var puzzle [9][9]square
 	for i, row := range puzzle {
 		for j := range row {
@@ -60,5 +136,6 @@ func (p Puzzle) createPuzzle() [9][9]square {
 			}
 		}
 	}
+	p.board = puzzle
 	return puzzle
 }
